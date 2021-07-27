aacacb55ab58aadd9a4c04ff23a13fdb222f3948
alexpataman[mac] <alex.pataman@gmail.com>
2021-07-27 13:11:15 +0200

    init: Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsPairs = bracketsConfig.reduce((acc, el) => (acc[el[0]] = el[1], acc), {})
+  let stack = [];
+  for (let item of str) {
+    if (item === bracketsPairs[stack[stack.length - 1]]) {
+      stack.pop();
+    } else if (bracketsPairs[item]) {
+      stack.push(item);
+    } else {
+      return false;
+    }
+  }
+  return !stack.length;
 }
